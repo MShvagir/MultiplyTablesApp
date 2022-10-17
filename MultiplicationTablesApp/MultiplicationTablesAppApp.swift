e968208d5aa0b4a0c7de825b4e70aba676539faa
Marina Shvagir <alira_m@mail.ru>
2022-10-17 10:18:07 +0300

    Initial Commit

---
+++ MultiplicationTablesApp/MultiplicationTablesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesAppApp.swift
+//  MultiplicationTablesApp
+//
+//  Created by Марина Швагирь on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
